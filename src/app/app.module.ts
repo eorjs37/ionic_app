fdf4ea0329cdcdd0ccc43bbc8602abd6223455f1
CHOI DAE GEON <chleorjs37@gmail.com>
2021-08-02 18:28:00 +0900

    LoginPageModule 삭제

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginPageModule } from './login/login.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,LoginPageModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
