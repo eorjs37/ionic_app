fc09414e531c4be749e5c72f639ffdd35752e791
CHOI DAE GEON <chleorjs37@gmail.com>
2021-10-08 09:00:48 +0900

    API 설정

---
+++ src/app/api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IonicUser } from './IonicUser';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  baseUrl = 'https://biggunsite.co.kr';
+  constructor(private http: HttpClient) { }
+  getUserAll() {
+    return this.http.get<IonicUser>(`${this.baseUrl}/api/ionic/user/all`);
+  }
+}
