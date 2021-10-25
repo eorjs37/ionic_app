81cbc3018a7bb5504ad092e37bb8a31fb46a227b
CHOI DAE GEON <chleorjs37@gmail.com>
2021-10-25 19:18:43 +0900

    모듈 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { File } from '@ionic-native/file/ngx';
+import { Media } from '@ionic-native/media/ngx';
+import { HTTP } from '@ionic-native/http/ngx';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Deeplinks } from '@ionic-native/deeplinks/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },File,Media,AndroidPermissions,HTTP,SplashScreen, StatusBar, Deeplinks],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
