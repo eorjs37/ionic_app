da559beacff693d81bd3885a09601e34b438fc93
CHOI DAE GEON <chleorjs37@gmail.com>
2021-10-30 08:37:16 +0900

    커밋

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(public platform: Platform,private router: Router,private _location: Location) {
+    
+    this.platform.ready().then(() => {
+      console.log('platform ready');
+      this.appExit();
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  appExit() {
+    this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
+      if (this._location.isCurrentPathEqualTo('/home')) {
+        console.log('Show Exit Alert!');
+        if (confirm('끄시겠습니까?')) {
+          navigator['app'].exitApp();
+        }
+      } else {
+         // Navigate to back page
+         console.log('Navigate to back page');
+         this._location.back();
+      }
+    });
+  }
 }
