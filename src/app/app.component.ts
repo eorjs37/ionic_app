27a33043ab31276ed9d429ef80dd38f34ded7e26
CHOI DAE GEON <chleorjs37@gmail.com>
2021-11-02 20:09:24 +0900

    권한 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component,OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(public platform: Platform,private router: Router,private _location: Location) {
+  constructor(public platform: Platform,private router: Router,private _location: Location, private speechRecognition: SpeechRecognition) {
     
     this.platform.ready().then(() => {
       console.log('platform ready');
@@ -17,11 +18,11 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.reqeustPermission();
   }
 
   appExit() {
-    this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
+    this.platform.backButton.subscribeWithPriority(10, () => {
       if (this._location.isCurrentPathEqualTo('/home')) {
         console.log('Show Exit Alert!');
         if (confirm('끄시겠습니까?')) {
@@ -33,5 +34,13 @@ export class AppComponent implements OnInit {
          this._location.back();
       }
     });
+  }
+
+  reqeustPermission() {
+    this.speechRecognition.requestPermission()
+      .then(
+         () => { console.log('Granted') }
+        ,() => { console.log('Denied') }
+      )
   }
 }
