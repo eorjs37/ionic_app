2fb31a2d6003ea2b2fd7efc6528829bd8e806ca2
CHOI DAE GEON <chleorjs37@gmail.com>
2021-11-20 09:39:00 +0900

    서비스 api 추가

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IonicUser } from './IonicUser';
+import { Messages } from './interface/messages';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  baseUrl:string = 'https://biggunsite.co.kr';
+  baseUrl: string = 'https://biggunsite.co.kr';
+  mockUrl: string = './assets/mock';
   constructor(private http: HttpClient) { }
   getUserAll() {
     return this.http.get<IonicUser>(`${this.baseUrl}/api/ionic/user/all`);
+  }
+
+  getMessages() {
+    return this.http.get<Messages[]>(`${this.mockUrl}/messages.json`)
+      .pipe(map((messages) => messages || []));
   }
 }
