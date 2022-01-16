7c7e6e96884d54f968f95f14e84e70d9d3baf2e8
chleorjs <chleorjs37@gmail.com>
2022-01-16 18:10:52 +0900

    슬라이드 추가

--- src/app/slider/component/slider1/slider1.component.ts
+++ src/app/slider/component/slider1/slider1.component.ts
@@ -2,8 +2,9 @@ import { Component,  EventEmitter,  Input,  OnInit, Output, SimpleChanges } from
 import { Howl } from 'howler';
 import { Store } from '@ngrx/store';
 import { recordingState } from '../../state/slider.actions';
-import { timer } from 'rxjs';
 import { SpeechService } from 'src/app/service/speech.service';
+import { TimerService } from 'src/app/service/timer.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-slider1',
   templateUrl: './slider1.component.html',
@@ -20,82 +21,42 @@ export class Slider1Component implements OnInit {
   milSecond: number = 0;
   timerObject: any;
   mySpeak: Array<string> = [];
+  $speech: Observable<any>;
+
   constructor(private store: Store,
-              private speechService: SpeechService) { }
+              private speechService: SpeechService,
+              private timerService: TimerService) { }
 
   ngOnInit(): void {
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.stopYn.currentValue === false) {
-      this.speechService.stopSpeech();
       this.store.dispatch(recordingState({ recordingState: false }));
       this.onChangeDetectEvent.emit();
       if (this.timerObject) {
         this.timerObject.unsubscribe();
       }
-    } else {
-      //this.startMusic();
-    }
+    } 
   }
 
-  startSpeech() {
-    this.speechService.startSpeech(this.options)
-      .subscribe(
-        (matches) => {
-          this.mySpeak.push(matches[0]);
-          this.keepRecording();
-        },
-        (error) => {
-          console.error(`startSpeech ${error}`);
-          this.mySpeak.push('');
-          this.keepRecording();
-        });
+  /**
+   * @description : 
+   */
+  onStartTimer() {
+    //this.$timer = this.timerService.startTimer();
+    
   }
 
-
-  keepRecording() {
-    if (this.milSecond >= 15) {
-      this.stopStt();
-      this.timerObject.unsubscribe();
-    } else {
-      this.startSpeech();
-    }
+  onStartSpeech() {
+    this.$speech = this.speechService.startSpeech();
   }
 
-  stopStt() {
-    console.log('mySpeak : ' , this.mySpeak);
-    this.store.dispatch(recordingState({ recordingState: false }));
-    this.onChangeDetectEvent.emit();
-    this.timerObject.unsubscribe();
-  }
-
-
-
-  startMusic() {
-    this.howl = new Howl({
-      src: ['./assets/music/mp3file.mp3'],
-      onplay: () => {
-        console.log('onplay');
-      },
-      onend: () => {
-        console.log('onend');
-      }
-    });
-    this.howl.play();
-  }
-
-  stopMusic() {
-    this.howl.stop();
-  }
-
-  startTimer() {
-    this.mySpeak = [];
-    this.store.dispatch(recordingState({ recordingState: true }));
-    const source = timer(/* 시작 초*/0, /* 초 간격 */1000);
-    this.timerObject = source.subscribe(val => {
-      this.milSecond = val;
-    });
-    this.startSpeech();
+  /**
+   * @description : stopTimer
+   */
+  onStopTimer() {
+    this.speechService.stopSpeech();
+    //this.timerService.stopTimer();
   }
 }
