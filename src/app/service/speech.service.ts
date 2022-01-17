d55c94fcd3f1f6626425f602f49090bca6e73eb9
chleorjs <chleorjs37@gmail.com>
2022-01-17 19:11:17 +0900

    스피치 서비스 추가

--- src/app/service/speech.service.ts
+++ src/app/service/speech.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SpeechRecognition } from '@awesome-cordova-plugins/speech-recognition/ngx';
 import { AlertService } from './alert.service';
+import { merge, of, Subject, timer } from 'rxjs';
+import { catchError, delay, delayWhen, map, retry, retryWhen, tap} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class SpeechService {
+  private $stopSubect = new Subject();
+  public sentence: string = '';
   constructor(private speechRecognition: SpeechRecognition,
     public platform: Platform,
     private alertService: AlertService) {
@@ -47,17 +51,63 @@ export class SpeechService {
     navigator['app'].exitApp();
   }
 
-  /**
-   * @description : startRecognition 시작
-   */
-  startSpeech(options: any) {
-    return this.speechRecognition.startListening(options)
+  speechRec() {
+    return this.speechRecognition.startListening({
+      'language': 'en-US',
+      'showPopup': false,
+      'showPartial': true
+    })
+  }
+
+  startSpeech() {
+    let startMil = 0;
+    const timer1 = timer(startMil, 1000);
+
+    this.sentence = '';
+
+    const speechrecognition = merge(timer1, this.speechRec())
+      .pipe(
+        map(x => {
+            startMil = typeof x === 'number' ? x : startMil;
+            return {
+              speech: typeof x === "object" ? this.combineStr(x) : this.sentence,
+              count: typeof x === 'number'? x : 0 
+            }
+          }),
+          tap(val => console.log('val1 : ', val)),
+          catchError(err => {
+            console.error('catchError : ', err);
+            console.log('startMil : ', startMil);
+            
+            return of([]) 
+          }),
+          retry(1)
+         
+          // retryWhen(errors =>
+          //   errors.pipe(
+          //     delayWhen(_ => this.speechRec())
+          //   )
+          // )
+      );
+
+    return speechrecognition;
+  }
+
+ 
+
+  stopSpeech() {
+    this.$stopSubect.next(true);
   }
 
   /**
-   * @description : stopRecognition 
+   * @description : 문장합치기
    */
-  stopSpeech() { 
-    return this.speechRecognition.stopListening();
+  private combineStr(arrayString: Array<string>) {
+    if (arrayString.length < 2) {
+      if (arrayString[0] !== '') {
+        this.sentence += arrayString[0];
+      }
+    }
+    return this.sentence;
   }
 }
