9121a19ceed043637a5f1274ff5963a9f9beb3a8
chleorjs <chleorjs37@gmail.com>
2022-02-21 20:06:44 +0900

    timer 계산

--- src/app/pipes/timer-expression.pipe.ts
+++ src/app/pipes/timer-expression.pipe.ts
@@ -7,7 +7,7 @@ export class TimerExpressionPipe implements PipeTransform {
 
   transform(timer: number): string {
     let mm = Math.floor(timer/60) > 0 ? `0${timer/60}` : '00';
-    let ss = timer % 60 > 10 ? timer % 60 : `0${timer % 60}`;
+    let ss = timer % 60 >= 10 ? timer % 60 : `0${timer % 60}`;
     
     return `${mm} : ${ss}`;
   }
