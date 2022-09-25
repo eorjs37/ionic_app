0164687d05ddd7199911c7d998d94151d67401b6
CHOI DAE GEON <chleorjs37@gmail.com>
2022-09-25 21:12:12 +0900

    로그인 처리

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,34 +1,31 @@
 import { Injectable } from '@angular/core';
-import { environment,SERVER_URL } from '@/environments/environment';
+import { SERVER_URL } from '@/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  httpOptions = { 
-    'Access-Control-Allow-Origin':'*',
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  httpOptions = {
+    'Access-Control-Allow-Origin': '*',
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   /**
    * @description : 커피 전체 조회
    */
-  getCoffAll(){
-    return this.http.get(SERVER_URL+'coffees/all',this.httpOptions);
+  getCoffAll() {
+    return this.http.get(SERVER_URL + 'coffees/all', this.httpOptions);
   }
 
   /**
    * @description 로그인
    * @param loginInfo 로그인정보
-   * @returns 
+   * @returns
    */
-  login(loginInfo){
-
-    return this.http.post(SERVER_URL+'ionic/user/login',loginInfo)
+  login(loginInfo) {
+    return this.http.post(SERVER_URL + 'ionic/user/login', loginInfo);
   }
 }
